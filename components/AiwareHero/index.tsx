97814185da56637f8ea3c885d3875b2f6e847b1c
aiwareai <piotrmacai0@gmail.com>
2024-10-25 20:55:51 +0200

    updated headings

--- components/AiwareHero/index.tsx
+++ components/AiwareHero/index.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
         <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0 flex flex-col md:flex-row"> {/* Added flex and flex-col for mobile view */}
           <div className="md:w-1/2 sm:w-full md:block mb-8 md:mb-0"> {/* Added mb-8 for mobile view */}
             <h4 className="text-black dark:text-white text-lg font-medium mb-4.5">
-              🔥 Hello! {t('we')} Insdr.Digital
+              Hello! {t('we')} Insdr 🔥
             </h4>
             <h1 className="text-black dark:text-white text-3xl xl:text-hero font-bold mb-5 pr-16">
                {t('mainheading')} 
