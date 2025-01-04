2c7af1f38afd19730fe1539d4fe75518b27f6ef0
Piotr Macai <macaipiotr@gmail.com>
2025-01-04 15:05:51 +0100

    updated agents

---
+++ app/(site)/voicebot/page.tsx
@@ -0,0 +1,219 @@
+'use client'
+
+import Image from "next/image";
+import SharePost from "@/components/Blog/SharePost";
+import RelatedPost from "@/components/Blog/RelatedPost";
+import { useTranslation } from 'react-i18next';
+import SupportBotFeaturesTab from "@/components/FeaturesTabAi/SupportBot";
+import FeaturesTab from "@/components/FeaturesTab";
+import Contact from "@/components/Contact";
+
+const VoiceBot = async () => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <title>{`Aiware - Support AI Assitant`}</title>
+      <section className="pt-35 lg:pt-45 xl:pt-50 pb-20 lg:pb-25 xl:pb-30">
+        <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
+
+
+
+            
+{/* FIRST ROW */}       
+          <div className="flex flex-col-reverse lg:flex-row gap-7.5 xl:gap-12.5">
+              <div className="lg:w-2/3">
+              <div className="animate_top rounded-md shadow-solid-13 bg-white dark:bg-blacksection border border-stroke dark:border-strokedark p-7.5 md:p-10">
+                        
+                <h2 className="font-semibold text-3xl 2xl:text-sectiontitle2 text-black dark:text-white mb-5">
+                {t('voicebot.header')}
+                </h2>
+                <div className="mb-10 w-full overflow-hidden ">
+           
+                  <div className="relative aspect-[97/60] w-full sm:aspect-[97/78]">
+                    <Image
+                      src={"/images/portfolio/voiceagent.png"}
+                      alt="Ai Tools Bot"
+                      fill
+                      className="object-contain object-center rounded-md"
+                    />
+                  </div>
+                </div>
+
+
+                <div className="blog-details">
+                  <p className="text-black dark:text-white">
+                  {t('voicebot.desc1')}                  
+                   </p>
+                  {/* <div className="relative aspect-[97/60] w-full sm:aspect-[97/78]">
+                    <Image
+                      src={"/images/portfolio/ainsidertoolsbot.jpg"}
+                      alt="Ai Tools Bot"
+                      fill
+                      className="object-contain object-center rounded-md"
+                    />
+                  </div> */}
+                  <p className="text-white dark:text-white">
+                    {t('voicebot.desc2')}
+                  </p>
+
+
+                  <p className="text-black dark:text-white">{t('supportbot.forwho')}</p> 
+                   <p className="text-black dark:text-white">{t('supportbot.who1')} </p>
+                   <p className="text-black dark:text-white"> {t('supportbot.who2')}</p>
+                   <p className="text-black dark:text-white"> {t('supportbot.who3')}</p>
+                   <p className="text-black dark:text-white"> {t('supportbot.who4')}</p>
+  
+                </div>
+
+                <a
+                  href="/contact"
+                  className="inline-flex items-center gap-2.5 font-medium text-white dark:text-black bg-black dark:bg-white rounded-full py-3 px-6 hover:opacity-90"
+                >
+                {t('cta.contact')}
+                  <Image
+                    width={20}
+                    height={20}
+                    src="/images/icon/icon-arrow-dark.svg"
+                    alt="Arrow"
+                    className="dark:hidden"
+                  />
+                  <Image
+                    width={20}
+                    height={20}
+                    src="/images/icon/icon-arrow-light.svg"
+                    alt="Arrow"
+                    className="hidden dark:block"
+                  />
+                </a>
+              </div>
+            </div>
+        
+            
+            <div className="md:w-1/2 lg:w-[32%]">
+            
+
+              <div className="animate_top rounded-md shadow-solid-13 bg-white dark:bg-blacksection border border-stroke dark:border-strokedark p-9 mb-10">
+                <h4 className="font-semibold text-2xl text-black dark:text-white mt-2 mb-7.5">
+                {/* {t('voicebot.details')} */}
+                Chat Engine
+                </h4>
+                <ul>      
+
+                    <li className="last:mb-0 mb-2 transition-all duration-300">
+                        <p className="Wtransition-all duration-300 hover:text-primary">Langchain</p>
+                    </li>
+                    <li className="last:mb-0 mb-2 transition-all duration-300">
+                        <p className="transition-all duration-300 hover:text-primary">Flowise</p>
+                    </li>
+                    <li className="last:mb-0 mb-2 transition-all duration-300">
+                        <p className=" transition-all duration-300 hover:text-primary">Voiceflow</p>
+                    </li>
+                    <li className="last:mb-0 mb-2 transition-all duration-300">
+                        <p className="transition-all duration-300 hover:text-primary">Botpress</p>
+                    </li>
+                </ul>
+              </div>
+
+              <div className="animate_top rounded-md shadow-solid-13 bg-white dark:bg-blacksection border border-stroke dark:border-strokedark p-9 mb-10">
+                <h4 className="font-semibold text-2xl text-black dark:text-white mt-2 mb-7.5">
+                {/* {t('voicebot.details')} */}
+                AI Engine
+                </h4>
+                <ul>      
+                <li className="last:mb-0 mb-2 transition-all duration-300">                    
+                  <p className="transition-all duration-300 hover:text-primary">OpenAI</p>
+                    </li>
+                    <li className="last:mb-0 mb-2 transition-all duration-300">
+                    <p className="transition-all duration-300 hover:text-primary">Claude Anthropic</p>
+                    </li>
+                    <li className="last:mb-0 mb-2 transition-all duration-300">
+                        <p className="transition-all duration-300 hover:text-primary">Meta LLama</p>
+                    </li>
+                    <li className="last:mb-0 mb-2 transition-all duration-300">
+                        <p className="transition-all duration-300 hover:text-primary">Mistral AI</p>
+                    </li>
+                </ul>
+              </div>
+
+              <div className="animate_top rounded-md shadow-solid-13 bg-white dark:bg-blacksection border border-stroke dark:border-strokedark p-9 mb-10">
+                <h4 className="font-semibold text-2xl text-black dark:text-white mt-2 mb-7.5">
+                  {t('voicebot.integrationsText')}
+                </h4>
+                <ul>
+                  <li className="last:mb-0 mb-2 transition-all duration-300">
+                    <p>Google Workspace</p></li>
+                    <li className="last:mb-0 mb-2 transition-all duration-300">
+                    <p>Google Drive</p>
+                  </li>
+                  <li className="last:mb-0 mb-2 transition-all duration-300">
+                    <p>Gmail</p>
+                  </li>
+                  <li className="last:mb-0 mb-2 transition-all duration-300">
+                    <p>Hubspot</p>
+                  </li>
+                  <li className="last:mb-0 mb-2 transition-all duration-300">
+                    <p>Airtable</p>
+                  </li>
+                  <li className="last:mb-0 mb-2 transition-all duration-300">
+                    <p>Notion</p>
+                  </li>
+                </ul>
+              </div>
+
+              
+              <div className="animate_top rounded-md shadow-solid-13 bg-white dark:bg-blacksection border border-stroke dark:border-strokedark p-9 mb-10">
+                <h4 className="font-semibold text-2xl text-black dark:text-white mt-2 mb-7.5">
+                  {t('voicebot.channels')}
+                </h4>
+                <ul>
+                <li className="last:mb-0 mb-3 transition-all duration-300">
+                    <p>{t('voicebot.web')}</p>
+                  </li>
+                  <li className="last:mb-0 mb-3 transition-all duration-300">
+                    <p>Telegram</p></li>
+                  <li className="last:mb-0 mb-3 transition-all duration-300">
+                    <p>Whatsapp</p>
+                  </li>
+                </ul>
+              </div>
+
+            </div>
+
+        </div>
+
+
+
+{/* SECOND ROW */}          
+<div className="flex flex-col-reverse lg:flex-row gap-7.5 xl:gap-12.5 mt-8">
+              <div className="animate_top rounded-md shadow-solid-13 bg-white dark:bg-blacksection border border-stroke dark:border-strokedark p-7.5 md:p-10">
+                        
+                <div className="mb-10 w-full overflow-hidden ">
+   
+                </div>
+
+
+                {/* <div className="blog-details">
+                   <p className="text-white transition-all duration-300 hover:text-primary"
+                  >{t('voicebot.desc2')}
+                  </p>
+                </div> */}
+
+                <div className="blog-details">
+                    < FeaturesTab />
+                </div>
+
+                <div className="blog-details">
+                    < Contact />
+                </div>
+
+                 </div>
+            </div>
+
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default VoiceBot;
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -224,7 +224,8 @@ const Header = () => {
          <Image src="/images/brand/hf-logo-pirate.svg" alt="git" width={24} height={24} className="fill-[#D1D8E0] hover:fill-primary transition-all duration-300"/>
          </a>
        </li>
-       <li>
+
+       {/* <li>
          <a href="https://www.behance.net/macaistudio" target="_blank">
            <svg
              className="fill-[#D1D8E0] hover:fill-primary transition-all duration-300"
@@ -237,8 +238,9 @@ const Header = () => {
              <path d="M22 7h-7v-2h7v2zm1.726 10c-.442 1.297-2.029 3-5.101 3-3.074 0-5.564-1.729-5.564-5.675 0-3.91 2.325-5.92 5.466-5.92 3.082 0 4.964 1.782 5.375 4.426.078.506.109 1.188.095 2.14h-8.027c.13 3.211 3.483 3.312 4.588 2.029h3.168zm-7.686-4h4.965c-.105-1.547-1.136-2.219-2.477-2.219-1.466 0-2.277.768-2.488 2.219zm-9.574 6.988h-6.466v-14.967h6.953c5.476.081 5.58 5.444 2.72 6.906 3.461 1.26 3.577 8.061-3.207 8.061zm-3.466-8.988h3.584c2.508 0 2.906-3-.312-3h-3.272v3zm3.391 3h-3.391v3.016h3.341c3.055 0 2.868-3.016.05-3.016z" />
            </svg>
          </a>
-       </li>
-       <li>
+       </li> */}
+
+       {/* <li>
          <a href="https://www.artstation.com/piotrmacai" target="_blank">
            <svg
              className="fill-[#D1D8E0] hover:fill-primary transition-all duration-300"
@@ -251,7 +253,8 @@ const Header = () => {
              <path d="M0 17.723l2.027 3.505h.001a2.424 2.424 0 0 0 2.164 1.333h13.457l-2.792-4.838H0zm24 .025c0-.484-.143-.935-.388-1.314L15.728 2.728a2.424 2.424 0 0 0-2.142-1.289H9.419L21.598 22.54l1.92-3.325c.378-.637.482-.919.482-1.467zm-11.129-3.462L7.428 4.858l-5.444 9.428h10.887z" />
            </svg>
          </a>
-       </li>
+       </li> */}
+
      </ul>
 
 
