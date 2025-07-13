706a7cd213101a697b5f3356d9a845a7fb0ecb51
Piotr Macai <piotrmacai0@gmail.com>
2025-07-13 18:36:43 +0200

    deplot error fix3

--- next.config.js
+++ next.config.js
@@ -14,6 +14,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "ainsider.co",
       },
+      {
+        protocol: "https",
+        hostname: "ainsider.store",
+      },
     ],
   },
 };
