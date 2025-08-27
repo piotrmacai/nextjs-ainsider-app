3399184835407f2dc43816d9bf57832881d09e7c
Piotr Macai <piotrmacai0@gmail.com>
2025-08-27 16:14:18 +0200

    updated feature image

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -8,9 +8,12 @@ export default function Head() {
       
       <meta property="og:type" content="website" />
       <meta property="og:title" content="Ainsider AI Company - AI | Web | Automations For Business" />
-      <meta property="og:description" content="Tailored To The Needs Of Your Company Web & AI and Automation solutions that help businesses to grow." />
-      <meta property="og:image" content='/images/logo/logo-white.png' />
+      <meta property="og:description" content="Fully Tailored To The Needs Of Your Company Web & AI and Automation solutions that help businesses to grow." />
+      <meta property="og:image" content={`${process.env.NEXT_PUBLIC_SITE_URL}/images/logo/logo-white.png`} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image" content={`${process.env.NEXT_PUBLIC_SITE_URL}/images/logo/logo-white.png`} />
     </>
   );
 }
- 
