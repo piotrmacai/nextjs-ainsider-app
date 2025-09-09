7f4cd07b7159eed1c4962853cc0e08c54137ee3f
Piotr Macai <piotrmacai0@gmail.com>
2025-09-09 02:12:35 +0200

    updaded faviconissies

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -5,21 +5,23 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Ainsider Company - AI | Web | Automations For Business" />
       <link rel="icon" href="/images/favicon.ico" />
-      
+      <link rel="icon" type="image/svg+xml" href="/images/favicon.svg" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+      <meta name="apple-mobile-web-app-title" content="Ainsider AI Company" />
+      <link rel="manifest" href="/manifest.json" />
   
 <meta property="og:locale" content="en_US" />
 <meta property="og:url" content="https://ainsider.co" />
 <meta property="og:type" content="website" />
 <meta property="og:title" content="Ainsider AI Company - AI | Web | Automations For Business" />
 <meta property="og:description" content="Tailored To The Needs Of Your Company Web & AI and Automation solutions that help businesses to grow." />
-{/* <meta property="og:image" content="https://ainsider.co/images/logo/ogimage.jpg" /> */}
-<meta name="og:image" content="https://opengraph.b-cdn.net/production/images/2bebd5f5-5663-4d6a-bf75-0b9e2fd1881b.jpg?token=bxCIjHY0uhOxk34x6CKH_12zn0s4g4-N62m5T9bRtfw&height=615&width=1200&expires=33292485479"/>
-<meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/2bebd5f5-5663-4d6a-bf75-0b9e2fd1881b.jpg?token=bxCIjHY0uhOxk34x6CKH_12zn0s4g4-N62m5T9bRtfw&height=615&width=1200&expires=33292485479"/>
+<meta property="og:image" content="https://ainsider.co/images/logo/ogimage.png" />
+<meta name="twitter:image" content="https://ainsider.co/images/logo/ogimage.png"/> 
 <meta property="og:image:width" content="1200" />
 <meta property="og:image:height" content="630" />
-<meta property="og:logo" content="https://ainsider.co/images/logo/webLogo.png" />
+<meta property="og:logo" content="https://ainsider.co/images/logo/logo-dark.png" />
 <meta name="twitter:card" content="summary_large_image" />
     </>
   );
 }
- 
